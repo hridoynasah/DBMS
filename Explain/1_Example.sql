2037d0d913cf02d19fab1aaca00a825fc3ec7c5f
Hridoy Hasan <hridoynasah@gmail.com>
2025-04-03 23:23:53 +0600

    author table

--- Explain/1_Example.sql
+++ Explain/1_Example.sql
@@ -1,9 +1,15 @@
 CREATE DATABASE IF NOT EXISTS myBook;
 SHOW DATABASES;
 USE myBook;
-CREATE TABLE Books(
+CREATE TABLE myBooks(
     ISBN VARCHAR(20) PRIMARY KEY,
     Title VARCHAR(50) NOT NULL,
     PublicationsYear INT,
     Summary TEXT
 )
+
+INSERT INTO Books(ISBN, Title, PublicationsYear, Summary) VALUES
+('978-0321765723', 'The Lord of the Rings', 1954, 'Epic fantacy tale..'),
+('978-0321765724', 'Harray Potter', 1954, 'Epic fantacy tale..');
+
+SELECT * FROM Books;
