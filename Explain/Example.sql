edbed04b2bc985d532c7bb7a1b714d00edc4aeb3
Hridoy Hasan <hridoynasah@gmail.com>
2025-04-03 22:55:07 +0600

    explanation

---
+++ Explain/Example.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS management;
+# IF NOT EXISTS -> Clause : same name er database already exists korle error theke prevent kore.
+
+USE management;
+# Switches the active database to management
+# jate kore sob command ei database er upor apply hoy
+
+CREATE TABLE Books(
+    ISBN 
+)
