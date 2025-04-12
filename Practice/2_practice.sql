b9bc2bb66db7d2b25454196263a893fed2b2a2b5
Hridoy Hasan <hridoynasah@gmail.com>
2025-04-13 01:55:38 +0600

    Enhance 2_practice.sql with additional UNION and RIGHT JOIN examples for better query demonstration

--- Practice/2_practice.sql
+++ Practice/2_practice.sql
@@ -4,3 +4,11 @@ SELECT s.LastName, s.Major, e.Grade
 FROM Students s
 LEFT JOIN Enrollments e
 ON s.StudentID = e.StudentID; 
+
+SELECT s.FirstName, s.LastName, e.Grade
+FROM Students s
+LEFT JOIN Enrollments e ON s.StudentID = e.StudentID
+UNION
+SELECT s.FirstName, s.LastName, e.Grade
+FROM Students s
+RIGHT JOIN Enrollments e ON s.StudentID = e.StudentID;
