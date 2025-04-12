6fb4703ab31c82835fa0260c6d6858ef56f382fc
Hridoy Hasan <hridoynasah@gmail.com>
2025-04-12 08:06:38 +0600

    Refactor Customer table operations and update queries

--- 2. MySQL Queries/1_create_database.sql
+++ 2. MySQL Queries/1_create_database.sql
@@ -12,3 +12,16 @@ INSERT INTO Customer(Name, Age) VALUES
 ('John', 23),
 ('Jerry', 21);
 
+SELECT * FROM Customer;
+
+UPDATE Customer SET Name = 'Tom' WHERE ID = 3;
+
+ALTER TABLE Customer ADD COLUMN bd_year INT;
+
+ALTER TABLE Customer DROP COLUMN bd_year;
+
+ALTER TABLE Customer MODIFY COLUMN bd_year DATE;
+
+ALTER TABLE Customer CHANGE COLUMN bd_year Birth_Date DATE;
+
+DELETE FROM Customer WHERE ID = 3;
